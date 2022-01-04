0ec9a951d1db8f8763f0d6911cfe01084faafc41
Kashish Choubey <kashishchoubey7516@gmail.com>
2022-01-04 13:17:50 +0530

    Deployment 4.0

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -19,7 +19,7 @@ import UserEditScreen from "./Screens/UserEditScreen";
 import ProductListScreen from "./Screens/ProductListScreen";
 import ProductEditScreen from "./Screens/ProductEditScreen";
 import OrderListScreen from "./Screens/OrderListScreen";
-import Dashboard from "./Dashboard";
+
 const App = () => {
   return (
     <Router>
@@ -57,7 +57,6 @@ const App = () => {
             component={HomeScreen}
           />
           <Route path='/Contact-Us' component={ContactUs} />
-          <Route path='/Dashboard' component={Dashboard} />
         </Container>
       </main>
       <Footer />
